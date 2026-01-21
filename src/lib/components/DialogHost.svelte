589e51819bcbf578da84d411c4afacf553dfd71b
Mallen220 <allenmc220@gmail.com>
2026-01-21 04:24:37 +0000

    Enhance plugin system with dialogs, timeline transformers, and field renderers

---
+++ src/lib/components/DialogHost.svelte
@@ -0,0 +1,13 @@
+<!-- Copyright 2026 Matthew Allen. Licensed under the Apache License, Version 2.0. -->
+<script lang="ts">
+  import { dialogRegistry } from "../registries";
+</script>
+
+{#each $dialogRegistry as dialog (dialog.id)}
+  {#if dialog.component}
+    <svelte:component
+      this={dialog.component}
+      {...dialog.props}
+    />
+  {/if}
+{/each}
