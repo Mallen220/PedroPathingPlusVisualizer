8c5745b0a7064f75db96f086710bf34211d9b5b7
Mallen220 <allenmc220@gmail.com>
2026-02-16 13:50:29 -0500

    Update license text to Modified Apache License
    Change license wording to "Modified Apache License, Version 2.0" across the codebase. Updated LICENSE and added LICENSE to NOTICE; updated header/copyright strings in example plugins, generated plugin types script, ExportCodeDialog, and fileHandlers. Also removed duplicate copyright comment lines in example plugins and applied a small formatting change in codeExporter (multiline ternary for degrees) for readability.

--- src/lib/components/dialogs/ExportCodeDialog.svelte
+++ src/lib/components/dialogs/ExportCodeDialog.svelte
@@ -167,7 +167,7 @@
             header: {
               info: "Created with Pedro Pathing Plus Visualizer",
               copyright:
-                "Copyright 2026 Matthew Allen. Licensed under the Apache License, Version 2.0.",
+                "Copyright 2026 Matthew Allen. Licensed under the Modified Apache License, Version 2.0.",
               link: "https://github.com/Mallen220/PedroPathingPlusVisualizer",
             },
             startPoint,
