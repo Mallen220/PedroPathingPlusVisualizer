8760f4b1e2ca877a071ee772e201adfd09aa2991
Mallen220 <allenmc220@gmail.com>
2026-03-22 20:17:25 -0400

    ugh

--- fix12.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let file = 'src/lib/components/FieldRenderer.svelte';
-let lines = fs.readFileSync(file, 'utf-8').split('\n');
-
-for (let i = 2370; i < 2405; i++) {
-    console.log(`${i + 1}: ${lines[i]}`);
-}
